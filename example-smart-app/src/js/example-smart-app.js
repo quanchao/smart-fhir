746ad560d217b58935421d891ad4ea2b75408ce1
vansia112 <55222588+vansia112@users.noreply.github.com>
2019-09-15 02:17:54 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -29,7 +29,9 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-          console.log(obv);
+          console.log(byCodes('8302-2'))
+          console.log(byCodes('29463-7'))
+          console.log(byCodes('2085-9'))
           var fname = '';
           var lname = '';
 
