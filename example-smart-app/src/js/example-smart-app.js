3eed03c0d6cb86b3820ac4d22b3cd8de1580f220
quanchao <lqc@gatech.edu>
2020-02-10 06:26:30 -0500

    added weight

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -18,7 +18,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|29463-7', 'http://loinc.org|29463-7']
                       }
                     }
                   });
@@ -31,6 +32,7 @@
           console.log(byCodes('8302-2'))
           console.log(byCodes('29463-7'))
           console.log(byCodes('2085-9'))
+          console.log(byCodes('29463-7'))
           var fname = '';
           var lname = '';
 
@@ -40,6 +42,7 @@
           }
 
           var height = byCodes('8302-2');
+           var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -52,6 +55,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+          p.weight = getQuantityValueAndUnit(weight[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -84,6 +88,7 @@
       gender: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
+      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
